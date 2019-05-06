8df79bb2ff73d9da3b6c4d834f451bd679af8708
ryanpetty <31255113+ryanpetty@users.noreply.github.com>
2019-05-06 00:46:22 -0400

    Add files via upload

---
+++ DeeperWoods/DeeperWoods/DeepWoods.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Monsters;
+using xTile;
+using xTile.Dimensions;
+
+namespace StardewValley.Locations
+{
+    class DeepWoods : GameLocation
+    {
+        public override bool answerDialogue(Response answer)
+        {
+            return base.answerDialogue(answer);
+        }
+
+        public override bool answerDialogueAction(string questionAndAnswer, string[] questionParams)
+        {
+            return base.answerDialogueAction(questionAndAnswer, questionParams);
+        }
+
+        public override bool canFishHere()
+        {
+            return base.canFishHere();
+        }
+
+        public override bool canSlimeHatchHere()
+        {
+            return base.canSlimeHatchHere();
+        }
+
+        public override bool canSlimeMateHere()
+        {
+            return base.canSlimeMateHere();
+        }
+
+        public override bool checkAction(Location tileLocation, xTile.Dimensions.Rectangle viewport, Farmer who)
+        {
+            return base.checkAction(tileLocation, viewport, who);
+        }
+
+        public override string checkForBuriedItem(int xLocation, int yLocation, bool explosion, bool detectOnly)
+        {
+            return base.checkForBuriedItem(xLocation, yLocation, explosion, detectOnly);
+        }
+
+        public override void checkForMusic(GameTime time)
+        {
+            base.checkForMusic(time);
+        }
+
+        public override void cleanupBeforePlayerExit()
+        {
+            base.cleanupBeforePlayerExit();
+        }
+
+        public override void cleanupBeforeSave()
+        {
+            base.cleanupBeforeSave();
+        }
+
+        public override void DayUpdate(int dayOfMonth)
+        {
+            base.DayUpdate(dayOfMonth);
+        }
+
+        public override void draw(SpriteBatch b)
+        {
+            base.draw(b);
+        }
+
+        public override void drawAboveAlwaysFrontLayer(SpriteBatch b)
+        {
+            base.drawAboveAlwaysFrontLayer(b);
+        }
+
+        public override void drawAboveFrontLayer(SpriteBatch b)
+        {
+            base.drawAboveFrontLayer(b);
+        }
+
+        public override void drawLightGlows(SpriteBatch b)
+        {
+            base.drawLightGlows(b);
+        }
+
+        public override void drawWater(SpriteBatch b)
+        {
+            base.drawWater(b);
+        }
+
+        public override bool dropObject(Object obj, Vector2 dropLocation, xTile.Dimensions.Rectangle viewport, bool initialPlacement, Farmer who = null)
+        {
+            return base.dropObject(obj, dropLocation, viewport, initialPlacement, who);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int getExtraMillisecondsPerInGameMinuteForThisLocation()
+        {
+            return base.getExtraMillisecondsPerInGameMinuteForThisLocation();
+        }
+
+        public override Object getFish(float millisecondsAfterNibble, int bait, int waterDepth, Farmer who, double baitPotency)
+        {
+            return base.getFish(millisecondsAfterNibble, bait, waterDepth, who, baitPotency);
+        }
+
+        public override Object getFish(float millisecondsAfterNibble, int bait, int waterDepth, Farmer who, double baitPotency, string locationName = null)
+        {
+            return base.getFish(millisecondsAfterNibble, bait, waterDepth, who, baitPotency, locationName);
+        }
+
+        public override int getFishingLocation(Vector2 tile)
+        {
+            return base.getFishingLocation(tile);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override Object getObjectAt(int x, int y)
+        {
+            return base.getObjectAt(x, y);
+        }
+
+        public override void hostSetup()
+        {
+            base.hostSetup();
+        }
+
+        public override bool isActionableTile(int xTile, int yTile, Farmer who)
+        {
+            return base.isActionableTile(xTile, yTile, who);
+        }
+
+        public override bool isCollidingPosition(Microsoft.Xna.Framework.Rectangle position, xTile.Dimensions.Rectangle viewport, bool isFarmer, int damagesFarmer, bool glider, Character character)
+        {
+            return base.isCollidingPosition(position, viewport, isFarmer, damagesFarmer, glider, character);
+        }
+
+        public override bool isCollidingPosition(Microsoft.Xna.Framework.Rectangle position, xTile.Dimensions.Rectangle viewport, bool isFarmer, int damagesFarmer, bool glider, Character character, bool pathfinding, bool projectile = false, bool ignoreCharacterRequirement = false)
+        {
+            return base.isCollidingPosition(position, viewport, isFarmer, damagesFarmer, glider, character, pathfinding, projectile, ignoreCharacterRequirement);
+        }
+
+        public override bool isObjectAt(int x, int y)
+        {
+            return base.isObjectAt(x, y);
+        }
+
+        public override bool isObjectAtTile(int tileX, int tileY)
+        {
+            return base.isObjectAtTile(tileX, tileY);
+        }
+
+        public override bool isTileLocationTotallyClearAndPlaceable(Vector2 v)
+        {
+            return base.isTileLocationTotallyClearAndPlaceable(v);
+        }
+
+        public override bool isTileLocationTotallyClearAndPlaceableIgnoreFloors(Vector2 v)
+        {
+            return base.isTileLocationTotallyClearAndPlaceableIgnoreFloors(v);
+        }
+
+        public override bool isTileOccupied(Vector2 tileLocation, string characterToIgnore = "")
+        {
+            return base.isTileOccupied(tileLocation, characterToIgnore);
+        }
+
+        public override bool isTileOccupiedForPlacement(Vector2 tileLocation, Object toPlace = null)
+        {
+            return base.isTileOccupiedForPlacement(tileLocation, toPlace);
+        }
+
+        public override bool isTileOccupiedIgnoreFloors(Vector2 tileLocation, string characterToIgnore = "")
+        {
+            return base.isTileOccupiedIgnoreFloors(tileLocation, characterToIgnore);
+        }
+
+        public override bool isTilePlaceable(Vector2 v, Item item = null)
+        {
+            return base.isTilePlaceable(v, item);
+        }
+
+        public override bool leftClick(int x, int y, Farmer who)
+        {
+            return base.leftClick(x, y, who);
+        }
+
+        public override void monsterDrop(Monster monster, int x, int y)
+        {
+            base.monsterDrop(monster, x, y);
+        }
+
+        public override bool performAction(string action, Farmer who, Location tileLocation)
+        {
+            return base.performAction(action, who, tileLocation);
+        }
+
+        public override void performTenMinuteUpdate(int timeOfDay)
+        {
+            base.performTenMinuteUpdate(timeOfDay);
+        }
+
+        public override bool performToolAction(Tool t, int tileX, int tileY)
+        {
+            return base.performToolAction(t, tileX, tileY);
+        }
+
+        public override void performTouchAction(string fullActionString, Vector2 playerStandingPosition)
+        {
+            base.performTouchAction(fullActionString, playerStandingPosition);
+        }
+
+        public override void removeEverythingExceptCharactersFromThisTile(int x, int y)
+        {
+            base.removeEverythingExceptCharactersFromThisTile(x, y);
+        }
+
+        public override void removeEverythingFromThisTile(int x, int y)
+        {
+            base.removeEverythingFromThisTile(x, y);
+        }
+
+        public override void seasonUpdate(string season, bool onLoad = false)
+        {
+            base.seasonUpdate(season, onLoad);
+        }
+
+        public override void shiftObjects(int dx, int dy)
+        {
+            base.shiftObjects(dx, dy);
+        }
+
+        public override bool shouldHideCharacters()
+        {
+            return base.shouldHideCharacters();
+        }
+
+        public override bool shouldShadowBeDrawnAboveBuildingsLayer(Vector2 p)
+        {
+            return base.shouldShadowBeDrawnAboveBuildingsLayer(p);
+        }
+
+        public override void spawnObjects()
+        {
+            base.spawnObjects();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public override void updateEvenIfFarmerIsntHere(GameTime time, bool ignoreWasUpdatedFlush = false)
+        {
+            base.updateEvenIfFarmerIsntHere(time, ignoreWasUpdatedFlush);
+        }
+
+        public override void updateMap()
+        {
+            base.updateMap();
+        }
+
+        public override void UpdateWhenCurrentLocation(GameTime time)
+        {
+            base.UpdateWhenCurrentLocation(time);
+        }
+
+        protected override void drawCharacters(SpriteBatch b)
+        {
+            base.drawCharacters(b);
+        }
+
+        protected override void drawFarmers(SpriteBatch b)
+        {
+            base.drawFarmers(b);
+        }
+
+        protected override LocalizedContentManager getMapLoader()
+        {
+            return base.getMapLoader();
+        }
+
+        protected override void initNetFields()
+        {
+            base.initNetFields();
+        }
+
+        protected override void resetLocalState()
+        {
+            base.resetLocalState();
+        }
+
+        protected override void resetSharedState()
+        {
+            base.resetSharedState();
+        }
+
+        protected override void updateCharacters(GameTime time)
+        {
+            base.updateCharacters(time);
+        }
+
+        protected override void updateWarps()
+        {
+            base.updateWarps();
+        }
+    }
+}
---
+++ DeeperWoods/DeeperWoods/ModEntry.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+
+using xTile;
+using xTile.Dimensions;
+using xTile.ObjectModel;
+using xTile.Tiles;
+
+namespace DeeperWoods
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod, IAssetLoader
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            // Adds button press to console
+            //helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+
+            // Adds a DeepWoods map
+            // the game clears locations when loading the save, so do it after the save loads
+            helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
+            
+            //Implemented using ContentPatcher instead
+            //GameEvents.UpdateTick += this.GameEvents_UpdateTick;
+        }
+
+        /// <summary>Get whether this instance can load the initial version of the given asset.</summary>
+        /// <param name="asset">Basic metadata about the asset being loaded.</param>
+        public bool CanLoad<T>(IAssetInfo asset)
+        {
+            return asset.AssetNameEquals("Maps/Woods");
+        }
+
+        /// <summary>Load a matched asset.</summary>
+        /// <param name="asset">Basic metadata about the asset being loaded.</param>
+        public T Load<T>(IAssetInfo asset)
+        {
+            return this.Helper.Content.Load<T>("assets/Woods.tbin");
+        }
+
+        /*********
+        ** Private methods
+        *********/
+        //Removed, no longer a necessary test feature
+        /*/// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+            // print button presses to the console window
+            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.");
+        }*/
+
+        private void OnSaveLoaded(object sender, SaveLoadedEventArgs args)
+        {
+            // load the DeepWoods map file from your mod folder
+            this.Helper.Content.Load<Map>("assets/DeepWoods.tbin", ContentSource.ModFolder);
+
+            // get the internal asset key for the DeepWoods map file
+            string mapAssetKey = this.Helper.Content.GetActualAssetKey("assets/DeepWoods.tbin", ContentSource.ModFolder);
+
+            // add the new location
+            GameLocation location = new GameLocation(mapAssetKey, "DeepWoods") { IsOutdoors = true, IsFarm = false };
+            Game1.locations.Add(location);
+        }
+
+        private void GameEvents_UpdateTick(object sender, EventArgs e)
+        {
+           //Implemented using ContentPatcher
+           /* if (Game1.currentLocation != null)
+            {
+                if (Game1.getCharacterFromName("Ent") == null)
+                {
+                    foreach (var i in Game1.locations)
+                        if (i.Name == "Town")
+                        {
+                            //public NPC(AnimatedSprite sprite, Vector2 position, int facingDir, string name, LocalizedContentManager content = null);
+                            var ent = new NPC(new AnimatedSprite("Characters\\Ent"), new Vector2(39, 58), 0, "Ent");
+                            //var ent = new EntNPC(new AnimatedSprite(Game1.content.Load<Texture2D>("Characters\\Ent"), 0, 24, Game1.tileSize * 2 / 4), new Vector2((float)(2 * Game1.tileSize), (float)(6 * Game1.tileSize)), "DeepWoods", 3, "Ent", false, (Dictionary<int, int[]>)null, Game1.content.Load<Texture2D>("Portraits\\Ent"));
+                            i.addCharacter(ent);
+                            ent.reloadSprite();
+                        }
+                }
+            }*/
+            var ent = new NPC(new AnimatedSprite("Characters\\Ent"), new Vector2(5, 22), 0, "Ent");
+            if (ent == null) Console.WriteLine("frick!");
+            if (Game1.getLocationFromName("Town") == null) Console.WriteLine("Frick again!");
+            Game1.getLocationFromName("Town").addCharacter(ent);
+            ent.reloadSprite();
+        }
+    }
+}
